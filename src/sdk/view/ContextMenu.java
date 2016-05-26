98a4437321185da336efe663b83f5e6e561fb2cf
Chris Jerrett <cjerrett18@stjohnsprep.org>
2016-05-25 21:15:32 -0400

    I blame the invisible hand

--- src/sdk/view/ContextMenu.java
+++ src/sdk/view/ContextMenu.java
@@ -11,29 +11,29 @@ import javax.swing.JPopupMenu;
 import sdk.core.Room;
 
 public class ContextMenu extends JPopupMenu {
-	
+
 	JMenuItem addRoomAbove;
 	JMenuItem addRoomBelow;
 	JMenuItem addRoomRight;
 	JMenuItem addRoomLeft;
-	
+
     public ContextMenu(RoomPanel room){
         addRoomAbove = new JMenuItem("Add Room Above");
         addRoomAbove.addActionListener(new ActionListener()
         {
 			@Override
-			public void actionPerformed(ActionEvent e) 
+			public void actionPerformed(ActionEvent e)
 			{
 				System.out.println("Adding room");
 				int id = Editor.getEditor().getRooms().size()-1;
 				Rectangle panelBounds = room.getBounds();
-				
+
 				int x = (int) panelBounds.getX();
 				int y = (int) (panelBounds.getY() - panelBounds.getHeight());
 				Rectangle bounds = new Rectangle(x,y, (int)panelBounds.getWidth(), (int)panelBounds.getHeight());
-				
+
 				Room room = new Room(id, bounds);
-				
+
 				RoomPanel panel = new RoomPanel(room);
 				panel.setBounds(bounds);
 				try {
@@ -42,16 +42,16 @@ public class ContextMenu extends JPopupMenu {
 					e1.printStackTrace();
 				}
 			}
-        	
+
         });
         add(addRoomAbove);
-        
+
         addRoomBelow = new JMenuItem("Add Room Below");
         add(addRoomBelow);
-        
+
         addRoomBelow = new JMenuItem("Add Room Right");
         add(addRoomBelow);
-        
+
         addRoomBelow= new JMenuItem("Add Room Left");
         add(addRoomBelow);
     }
