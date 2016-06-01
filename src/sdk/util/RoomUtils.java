7a9303603e0d040dd73476c8b54b5d3850e9cbee
Chris Jerrett <cjerrett18@stjohnsprep.org>
2016-06-01 02:27:36 -0400

    I have to sleep

--- src/sdk/util/RoomUtils.java
+++ src/sdk/util/RoomUtils.java
@@ -1,7 +1,14 @@
 package sdk.util;
 
+import java.awt.Color;
 import java.awt.Point;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
+
+import sdk.core.Room;
+import sdk.core.Room.Floors;
+import sdk.core.Room.Walls;
 import sdk.view.DungeonEditor;
 import sdk.view.RoomPanel;
 
@@ -11,6 +18,56 @@ import sdk.view.RoomPanel;
  */
 public class RoomUtils
 {
+
+	public static BufferedImage genKeyBufferedImageFromRoom(Room room)
+	{
+		BufferedImage bi = new BufferedImage(16, 9, BufferedImage.TYPE_INT_RGB);
+
+		int wall = new Color(room.getWall().id, room.getWall().id, 255).getRGB();
+		int tile = new Color(room.getFloor().id, room.getFloor().id, 0).getRGB();
+
+		for(int row = 0; row < bi.getHeight(); row++) {
+			for(int col = 0; col < bi.getWidth(); col++) {
+				if(row != 0 && row != bi.getHeight()-1 && col != 0 && col != bi.getWidth()-1)
+				{
+					bi.setRGB(col, row, tile);
+				}
+				else {
+					bi.setRGB(col, row, wall);
+				}
+			}
+		}
+		if(room.getTop() != -1)
+		{
+			for(int i = 7; i < 9; i++)
+			{
+				bi.setRGB(i, 0, TILE);
+			}
+		}
+
+		if(room.getBottom() != -1)
+		{
+			for(int i = 7; i < 9; i++)
+			{
+				bi.setRGB(i, bi.getHeight()-1, TILE);
+			}
+		}
+
+		if(room.getLeft() != -1)
+		{
+			for(int i = 3; i < 6; i++) {
+				bi.setRGB(0, i, TILE);
+			}
+		}
+
+		if(room.getRight() != -1)
+		{
+			for(int i = 3; i < 6; i++) {
+				bi.setRGB(bi.getWidth()-1, i, TILE);
+			}
+		}
+		return bi;
+	}
 
 	/**
 	 * Finds what roompanel a mouse is on
@@ -22,7 +79,7 @@ public class RoomUtils
 	{
 		for(RoomPanel room: DungeonEditor.getEditor().getRooms())
 		{
-			
+
 			if(room.getBounds().contains(new Point(x,y)))
 			{
 				return room;
